a1feb57a587d871d090efd0f9593d92e3b5084de
SejalMahajan <sejalmahajan2008@gmail.com>
2024-06-09 22:56:27 +0530

    updated photo

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -107,7 +107,7 @@ const Sidebar = () => {
                   fontWeight="bold"
                   sx={{ m: "10px 0 0 0" }}
                 >
-                  Aditya Jain
+                  Sejal Mahajan
                 </Typography>
                 <Typography variant="h5" color={colors.greenAccent[500]}>
                   Admin Panel
